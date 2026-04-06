8ad48ad02f51ed3f9aaadb098ffb706b8a2899cb
capad1946 <wizard1946@163.com>
2026-04-06 11:27:29 +0800

    更新样式

---
+++ client/OpenTalk/Views/Components/ChatMessageBubble.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace OpenTalk.Views.Components;
+
+public partial class ChatMessageBubble : UserControl
+{
+    public ChatMessageBubble()
+    {
+        InitializeComponent();
+    }
+}
